830421095d2f8235ffe3a980ab48bd7324055ea2
norestrictionss <girayakman@gmail.com>
2023-04-14 01:00:46 +0300

    Files created

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        string first_element = strs[0];
+        string result = "";
+
+        if(strs.size()==1)
+            return first_element;
+
+
+
+        int j = 0;
+        int i = 0;
+        bool isCommon = true;
+
+        
+        while(j<strs[i].length() && j<strs[i+1].length()){
+
+
+            if(strs[i][j] != strs[i+1][j])
+                return result;
+            
+
+
+            if(i==strs.size()-2){
+
+                if(isCommon)
+                    result += strs[i][j];
+                
+                i = 0;
+                j++;
+
+            }
+            else i++;
+
+        }
+        
+        
+
+        return result;
+        
+    }
+};
+
+int main(){
+	
+	
+	Solution solution = Solution();
+	vector<string> inputs;
+	inputs.push_back("flower");
+	inputs.push_back("flow");
+	inputs.push_back("flight");
+	cout<<solution.longestCommonPrefix(inputs);
+	
+	return 0;
+}
