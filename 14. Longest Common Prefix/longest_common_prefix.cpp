c66557432f714d0cfeac4e62b0625a668c38694e
norestrictionss <girayakman@gmail.com>
2023-07-04 21:11:58 +0300

    Edited files

--- 14. Longest Common Prefix/longest_common_prefix.cpp
+++ 14. Longest Common Prefix/longest_common_prefix.cpp
@@ -10,47 +10,31 @@ public:
 
         string first_element = strs[0];
         string result = "";
-
         if(strs.size()==1)
             return first_element;
-
-
-
         int j = 0;
         int i = 0;
-        bool isCommon = true;
-
-        
         while(j<strs[i].length() && j<strs[i+1].length()){
-
 
             if(strs[i][j] != strs[i+1][j])
                 return result;
-            
-
 
             if(i==strs.size()-2){
 
-                if(isCommon)
-                    result += strs[i][j];
+                result += strs[i][j];
                 
                 i = 0;
                 j++;
 
             }
             else i++;
-
         }
-        
-        
 
         return result;
-        
     }
 };
 
 int main(){
-	
 	
 	Solution solution = Solution();
 	vector<string> inputs;
